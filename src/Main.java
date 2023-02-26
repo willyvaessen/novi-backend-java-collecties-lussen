91326ea8d735f2d12f6d3506acdefc9af85f4740
willyvaessen <github@willyvaessen.nl>
2023-02-26 18:24:32 +0100

    Uitwerking branch aangemaakt.

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        // Hier mag je je code scrijven voor de hoofd-opdracht
+        // Hier mag je je code schrijven voor de hoofd-opdracht
+        //  2.  Maak in je main methode een Integer array genaamd numeric die je vult met de nummers 1,2,3,4,5,6,7,8,9,0.
+        Integer [] numeric = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+
+        //  3.  Maak in je main methode een String array genaamd alphabetic die je vult met de
+        //  String varianten van de nummers uit stap 1, dus: "een", "twee", ... etc ..., "negen", "nul".
+        String [] alphabetic = {"een", "twee", "drie", "vier", "vijf", "zes", "zeven", "acht", "negen", "nul"};
+
+
+        //  4.  Maak een nieuwe class aan en noem deze Translator (zie Class Translator)
+        //  9.  De constructor is klaar. Nu ga je deze aanroepen met de juiste argumenten in de main methode
+        //      van de Main class, oftewel: maak in main een nieuw object aan van het type Translator.
+        Translator translator = new Translator(alphabetic, numeric);
+
+
 
         /* deze regel mag je weg halen voor de bonus opdracht. Onderstaande code is voor de bonus opdracht.
         HashSet<Integer> secretnumber = randomnumbergenerator();
@@ -19,6 +33,8 @@ public class Main {
     /*
      Deze methode is voor de bonus opdracht.
      */
+
+    //  Methode feedback
     public static void feedback(String stringnumber) {
         Scanner scanner = new Scanner(System.in);
         StringBuilder feedback = new StringBuilder();
